9d0be1f4ccd0e67c1bd389e5d763ce7e2765cb04
Meezo <mozeiny@gmail.com>
2018-12-16 06:50:41 -0800

    added React and modified boiler plate code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const dbConfig = {
 };
 
 // Setting up Server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`))
 
 //Function to connect to database and execute query
